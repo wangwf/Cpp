a2fb51457bcc462763dee2814845bf28d18bc8e8
Wenfeng <wenfeng@venor.co>
2014-03-14 17:36:17 -0700

    dataWrangle

---
+++ dataWrangleMongoDB/openStreepMap.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Your task is to use the iterative parsing to process the map file and
+find out not only what tags are there, but also how many, to get the
+feeling on how much of which data you can expect to have in the map.
+The output should be a dictionary with the tag name as the key
+and number of times this tag can be encountered in the map as value.
+
+Note that your code will be tested with a different data file than the 'example.osm'
+"""
+import xml.etree.ElementTree as ET
+import pprint
+
+def count_tags(filename):
+        # YOUR CODE HERE
+        tree = ET.parse(filename)
+        root=tree.getroot()
+        output={}
+        for t in root:
+            if t.tag in output.keys():
+                output[t.tag] +=1
+            else:
+                output[t.tag]=1
+        return output
+        
+
+def test():
+
+    tags = count_tags('example.osm')
+    pprint.pprint(tags)
+    assert tags == {'bounds': 1,
+                     'member': 3,
+                     'nd': 4,
+                     'node': 20,
+                     'osm': 1,
+                     'relation': 1,
+                     'tag': 7,
+                     'way': 1}
+
+
+if __name__ == "__main__":
+    test()
+
+
+import xml.etree.ElementTree as ET
+import pprint
+from collections import defaultdict
+import re
+
+osm_file=open("chicage_tail.osm", "r")
+
+street_type_re = re.compile(r'\b\S+\.?$', re.IGNORECASE)
+street_types = defaultdict(set)
+
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place","Way"]
+def audit_street_type(street_types, street_name):
+    m = street_type_re.search(street_name)
+    if m:
+        street_type = m.group()
+        if street_type not in expected:
+            street_types[street_type].add(street_name)
+
+def print_sorted_dict(d):
+    keys = d.keys()
+    keys = sorted(keys, key=lambda s: s.lower())
+    for k in keys:
+        v = d[k]
+        print "%s: %d" %(k,v)
+
+def is_street_name(elem):
+    return (element.attrib['k'] == "addr:street")
+
+def audit():
+    for event, elem in ET.iterparse(osm_file, events=("start",)):
+        if elem.tag == "way":
+            for tag in elem.iter("tag"):
+                if is_street_name(tag):
+                    audit_street_type(street_types, tag.attrib['v'])
+    pprint.pprint(dict(street_types))
+
+if __name__ == '__main__':
+    audit()
+
+
+
+
+import xml.etree.ElementTree as ET
+import pprint
+import re
+"""
+Your task is to explore the data a bit more.
+Before you process the data and add it into MongoDB, you should
+check the "k" value for each "<tag>" and see if they can be valid keys in MongoDB,
+as well as see if there are any other potential problems.
+
+We have provided you with 3 regular expressions to check for certain patterns
+in the tags. As we saw in the quiz earlier, we would like to change the data model
+and expand the "addr:street" type of keys to a dictionary like this:
+{"address": {"street": "Some value"}}
+So, we have to see if we have such tags, and if we have any tags with problematic characters.
+Please complete the function 'key_type'.
+"""
+
+
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+
+def key_type(element, keys):
+    #print keys
+    if element.tag == "tag":
+        # YOUR CODE HERE
+         for tag in element.iter("tag"):
+                key=tag.attrib['k']
+                if lower.search(key):
+                    keys['lower'] +=1
+                elif lower_colon.search(key):
+                    keys['lower_colon'] +=1
+                elif problemchars.search(key):
+                    keys['problemchars'] +=1
+                else:
+                    keys['other'] +=1
+        
+    return keys
+
+
+
+def process_map(filename):
+    keys = {"lower": 0, "lower_colon": 0, "problemchars": 0, "other": 0}
+    for _, element in ET.iterparse(filename):
+        keys = key_type(element, keys)
+
+    return keys
+
+
+
+def test():
+    # You can use another testfile 'map.osm' to look at your solution
+    # Note that the assertions will be incorrect then.
+    keys = process_map('example.osm')
+    pprint.pprint(keys)
+    assert keys == {'lower': 5, 'lower_colon': 0, 'other': 1, 'problemchars': 1}
+
+
+if __name__ == "__main__":
+    test()
